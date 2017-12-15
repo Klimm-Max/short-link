424c692e0e8cd0d775a213d93598cf2de96f90a8
Max Klimm <maxklimm@Maxs-MBP.home>
2017-12-15 17:11:19 +0100

    refactored React.Components to Stateless functional comp.

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { browserHistory } from '../../client/main';
 
-import LinksList from './LinksList';
 import Header from './components/Header';
+import LinksList from './LinksList';
 import AddLinkForm from './AddLinkForm';
 
 class Link extends React.Component {
   
   componentDidMount() {
+    // if there is no logged-in user -> redirect to the root application
     if (!Meteor.userId()) {
       browserHistory.replace('/');
     }
--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-
-  render() {
-    return(
-      <p>Not Found 404</p>
-    );
-  }
-
+export default () => {
+  return(
+    <p>Not Found - 404</p>
+  );
 }
