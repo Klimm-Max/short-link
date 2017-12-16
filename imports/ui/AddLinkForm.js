c1c6b8e3edb992715e4c9268e881484a375d5b2c
Max Klimm <maxklimm@Maxs-MBP.home>
2017-12-17 00:45:35 +0100

    refactored AddLinkForm to a controlled component

--- imports/ui/AddLinkForm.js
+++ imports/ui/AddLinkForm.js
@@ -3,15 +3,29 @@ import { Meteor } from 'meteor/meteor';
 
 export default class AddLinkForm extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    }
+  }
+
   onSubmit(e) {
-    const url = this.refs.url.value.trim();
+    const url = this.state.url;
 
     e.preventDefault();
 
     if (url) {
-      Meteor.call('links.insert', url);
-      this.refs.url.value = '';
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({ url: '' });
+        }
+      });
     }
+  }
+
+  onChange(e) {
+    this.setState({ url: e.target.value.trim() });
   }
 
   render() {
@@ -19,7 +33,12 @@ export default class AddLinkForm extends React.Component {
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"></input>
+          <input 
+            type="text" 
+            ref="url" 
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}/>
           <button>Add Link</button>
         </form>
       </div>
