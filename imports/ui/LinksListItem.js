0d42603e1cab495824f5950c4bd0dabbcb12ef79
Max Klimm <maxklimm@Maxs-MBP.home>
2017-12-17 21:04:17 +0100

    Added LastVisited Feature for links

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
 
@@ -28,16 +29,27 @@ export default class LinksListItem extends React.Component {
   componentWillUnmount() {
     this.clipboard.destroy();
   }
+
+  renderStats() {
+    const { visitedCount, lastVisitedAt } = this.props;
+    const visitMessage = visitedCount === 1 ? 'visit' : 'visits';
+    let visitedMessage = null;
+    
+    if (typeof lastVisitedAt === 'number') {
+      visitedMessage = `- (visited ${ moment(lastVisitedAt).fromNow() })`;
+    }
+
+    return <p>{visitedCount} {visitMessage} {visitedMessage}</p>
+  }
   
   render() {
-    const { shortUrl, url, _id, visible, visitedCount, lastVisitedAt } = this.props;
+    const { shortUrl, url, _id, visible } = this.props;
     return(
       <div>
         <p>{url}</p>
         <p>{shortUrl}</p>
-        <p>Visited {visitedCount}{visitedCount==1 ? ' time':' times'}</p>
-        <p>{lastVisitedAt}</p>
-        
+        {this.renderStats()}
+
         <button ref="copy" data-clipboard-text={shortUrl}>
           { this.state.justCopied ? 'Copied' : 'Copy' }
         </button>
